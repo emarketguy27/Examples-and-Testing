95cb970808ece1ac14c5f59db0551578f0efd6cd
emarketguy27 <j.dennisuk@gmail.com>
2024-11-04 15:39:48 +0100

    Added minor styling changes and additional 'Default Theme' option.

--- rotary.js
+++ rotary.js
@@ -3,7 +3,7 @@ $(".select").click(function () {
     case $(this).hasClass("one"):
       $(".switch").css("-webkit-transform", "rotate(-50deg)");
       $(".one").addClass("selected");
-      $(".two, .three").removeClass("selected");
+      $(".two, .three, .off").removeClass("selected");
       $("body").addClass(".mid-theme");
       $("html").attr("theme", "low");
       break;
@@ -11,7 +11,7 @@ $(".select").click(function () {
     case $(this).hasClass("two"):
       $(".switch").css("-webkit-transform", "rotate(0deg)");
       $(".two").addClass("selected");
-      $(".one, .three").removeClass("selected");
+      $(".one, .three, .off").removeClass("selected");
       $("body").removeClass(".mid-theme");
       $("html").attr("theme", "mid");
       break;
@@ -19,8 +19,15 @@ $(".select").click(function () {
     case $(this).hasClass("three"):
       $(".switch").css("-webkit-transform", "rotate(50deg)");
       $(".three").addClass("selected");
-      $(".one, .two").removeClass("selected");
+      $(".one, .two, .off").removeClass("selected");
       $("html").attr("theme", "high");
+      break;
+
+    case $(this).hasClass("off"):
+      $(".switch").css("-webkit-transform", "rotate(180deg)");
+      $(".off").addClass("selected");
+      $(".one, .two, .three").removeClass("selected");
+      $("html").attr("theme", "");
       break;
 
     default: //Do nothing.
